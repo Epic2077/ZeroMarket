a842c6123fa8b60b100749564cc89d515c8a328e
Ashkan <the.epic.2077@gmail.com>
2026-08-30 13:54:46 +0330

    fix: test supabase

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8000",
+        protocol: "https",
+        hostname: "ibyutzpibfuejzzvktow.supabase.co",
+
         pathname: "/storage/v1/object/public/**",
       },
     ],
