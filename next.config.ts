3eee67aa4ad6912a3fc04a19801bb8a14efb0887
Ashkan <the.epic.2077@gmail.com>
2026-09-01 19:37:16 +0330

    feat: new sharing system with image and link
    fix: some bugs are fixed too

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,12 @@ const nextConfig: NextConfig = {
         hostname: "ibyutzpibfuejzzvktow.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
+      {
+        protocol: "http",
+        hostname: "92.114.51.99",
+        port: "8000",
+        pathname: "/storage/v1/object/public/**",
+      },
     ],
   },
 };
